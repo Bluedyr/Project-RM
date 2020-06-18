103cfffc15b5da4fafc791e4e58b77981a695c43
Bluedyr <42643249+Bluedyr@users.noreply.github.com>
2020-06-18 18:08:15 +1200

    Started Smoke Bomb ability
    Created an empty ability for #3

---
+++ Assets/Scripts/New Controller/Power System/SmokeBomb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Abilities/SmokeBomb")]
+public class SmokeBomb : Ability{
+
+    public override void Initialise(GameObject obj) {
+        
+    }
+
+    public override void Update() {
+
+    }
+
+    public override void OnDown() {
+
+
+    }
+
+    public override void OnHold() {
+      
+
+    }
+
+    public override void OnUp() {
+;
+
+    }
+}
