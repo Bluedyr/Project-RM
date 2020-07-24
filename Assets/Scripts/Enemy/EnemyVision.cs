281bad21e2c17adb00ec0e50b79eb4b743a54631
Bluedyr <42643249+Bluedyr@users.noreply.github.com>
2020-07-24 20:24:34 +1200

    Enemy Vision started
    #2 need to add bones to the player

---
+++ Assets/Scripts/Enemy/EnemyVision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVision : MonoBehaviour{
+
+
+    public Transform playerCameraTransform;
+
+    public float fov = 45f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+
+
+
+    }
+
+    void seePlayer() {
+        if (Vector3.Angle(transform.forward, playerCameraTransform.position) <= fov) {
+
+
+            //TODO
+            //Add "bones" for the enemy to see
+            //raycast to each player "bone"
+            //We only raycast the camera is in view. My thinking is that it will seem more fair to the player (i.e poking from behind corner slightly won't get you caught unless you can see the enemy)
+        }
+    }
+}
